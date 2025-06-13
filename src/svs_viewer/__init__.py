6dd31d3726d3b3c44e44042674f0800cda140609
Taosha <taosha.gao@utsouthwestern.edu>
2025-06-13 11:18:20 -0500

    updates

--- src/svs_viewer/__init__.py
+++ src/svs_viewer/__init__.py
@@ -7,6 +7,8 @@ except ImportError:
 from ._reader import napari_get_reader
 from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
 from ._writer import write_multiple, write_single_image
+from .utils import rgb2hed, threshold_nuclei_dask
+from .stardist_he import predict_nucleus
 
 __all__ = (
     "napari_get_reader",
@@ -16,4 +18,7 @@ __all__ = (
     "ImageThreshold",
     "threshold_autogenerate_widget",
     "threshold_magic_widget",
+    "rgb2hed",
+    "threshold_nuclei_dask",
+    "predict_nucleus"
 )
