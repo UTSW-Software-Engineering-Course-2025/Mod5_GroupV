f2abf473f4f36756c2526a56fe73e1146193750b
Yuyan Liu <yuyanmaggieliu@gmail.com>
2025-06-13 01:00:48 -0400

    Work on layer plugin

--- src/svs_viewer/utils.py
+++ src/svs_viewer/utils.py
@@ -1,5 +1,5 @@
 import dask.array as da
-
+import numpy as np
 rgb_from_hed = np.array([
     [0.65, 0.70, 0.29],
     [0.07, 0.99, 0.11], 
@@ -16,3 +16,11 @@ def rgb2hed(rgb):
     stains = (da.log(rgb) / log_adjust) @ hed_from_rgb
     da.maximum(stains, 0, out=stains) # clip zeros
     return stains
+
+def threshold_nuclei_dask(hed_image_dask_array, threshold_value=0.001):
+    """
+    Thresholds the Hematoxylin channel of an HED Dask array to identify nuclei.
+    """
+    hematoxylin_channel = hed_image_dask_array[:, :, 0] # Assuming Hematoxylin is the first channel
+    nuclei_mask = (hematoxylin_channel > threshold_value).astype(np.uint8)
+    return nuclei_mask
